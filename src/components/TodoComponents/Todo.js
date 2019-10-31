103c411cb2934d3c9d5b04b2da673be1d87623dd
Austin Burton <abur3460@gmail.com>
2019-10-30 19:43:22 -0700

    finished styling, i am done

--- src/components/TodoComponents/Todo.js
+++ src/components/TodoComponents/Todo.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import "./Todo.css";
+import { InlineFunctions } from 'terser';
 
 const Todo = props => {
     return (
-        <div
-            style={props.todo.completed ? { textDecoration: 'line-through' , color: '#4bafb8'} : null}
+        <div className="list-style"
+            style={props.todo.completed ? {
+                color: 'white',
+            } : null}
             onClick={() => props.handleToggleComplete(props.todo.id)}
         >
             {props.todo.task}
+            <i class="fas fa-check" style={props.todo.completed ? {
+                display: 'inline',
+                paddingLeft: '5px',
+                color: 'black',
+            } : null}></i>
         </div>
+
     );
 };
 
